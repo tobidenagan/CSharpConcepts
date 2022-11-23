7c5c4e025c5841677743957cb4c3db4f18d9fe87
Tobi Denagan <denagantobi@gmail.com>
2022-11-23 14:16:50 +0100

    Add project files.

---
+++ DataStructures/DictionaryUsage.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpConcepts.DataStructures
+{
+    public static class DictionaryUsage
+    {
+        public static void DictionaryPractice()
+        {
+            List<string> list = new() { "one", "two", "three", "four" };
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                dict.Add(i, list[i]);
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"Key is {item.Key}, Value is {item.Value}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpConcepts.DataStructures;
+
+DictionaryUsage.DictionaryPractice();
