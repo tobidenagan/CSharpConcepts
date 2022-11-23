acd9af75ba3a874baa47f9eab6e71f05a30602d7
Tobi Denagan <denagantobi@gmail.com>
2022-11-23 14:49:52 +0100

    DictionaryUsage update

--- DataStructures/DictionaryUsage.cs
+++ DataStructures/DictionaryUsage.cs
@@ -6,14 +6,31 @@
         {
             List<string> list = new() { "one", "two", "three", "four" };
             Dictionary<int, string> dict = new Dictionary<int, string>();
-            for (int i = 0; i < list.Count; i++)
+
+            //you can use this
+
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    dict.Add(i, list[i]);
+            //}
+
+            //you can use this also
+
+            int i = 0;
+            foreach (var item in list)
             {
-                dict.Add(i, list[i]);
+                if(!dict.TryGetValue(i, out var value))
+                {
+                    dict.Add(i, list[i]);
+                    ++i; //or i++ (first use the value before incrementing it(left to right execution))
+                }
             }
+
             foreach (var item in dict)
             {
                 Console.WriteLine($"Key is {item.Key}, Value is {item.Value}");
             }
+            Console.WriteLine(dict[0]);
         }
     }
 }
---
+++ DataStructures/HashtableUsage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts.DataStructures
+{
+    public class HashtableUsage
+    {
+        public static void HashtablePractice()
+        {
+            List<string> list = new() { "one", "two", "three", "four" };
+            Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < list.Count; i++)
+            {
+                hashtable.Add(i, list[i]);
+            }
+            hashtable.Add("testValue", hashtable.ToString());
+            foreach (var item in hashtable)
+            {
+                //Console.WriteLine($"Key is {item.}, Value is {item.Value}");
+            }
+        }
+    }
+}
